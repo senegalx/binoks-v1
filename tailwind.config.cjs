17a37d0bed6d2cf7ca2f9e8c9f532d0f4be56c26
Mamadou Diagne <genova@dofbi.com>
2023-09-28 16:07:43 +0000

    💄 style (Binoks):  new

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,8 +4,8 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        customPrimary: "rgb(99, 102, 241)",
-        customSecondary: "rgb(161, 163, 247)",
+        customPrimary: "#B9D660",
+        customSecondary: "#B9D662",
         customDarkBg1: "rgb(31, 32, 35)",
         customDarkBg2: "rgb(38, 39, 43)",
         customDarkBg3: "rgb(48, 49, 54)",
